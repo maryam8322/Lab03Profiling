a0efe01fd2e4802080d54dcfc7d1987db9d8f6ec
maryam8322 <82034522+maryam8322@users.noreply.github.com>
2022-02-16 00:44:50 +0000

    Add files via upload

---
+++ CollatzSequence.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+/* the function return number based on even/odd test*/
+int test(unsigned int x)
+{
+    /* write your code here*/
+}
+
+/* the function return the length of the sequnce*/
+int count_length(unsigned int x)
+{
+    /* write your code here*/
+}
+
+int main(int argc, char* argv[])
+{
+    unsigned int i, init = 0, count = 0;
+
+    for (i = 1; i < 100000000; i++)
+    {
+        unsigned int k = count_length(i);
+
+        if (k > init)
+        {
+            init = k;
+            count = i;
+            printf("The sequence length for the number %u equals %u \n", init, count);
+        }
+    }
+
+    return 0;
+}
